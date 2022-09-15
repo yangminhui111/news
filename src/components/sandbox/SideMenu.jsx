3e2031d12c9022b8e82b9c2b702548b8d5a168ce
unknown <1497972988@qq.com>
2022-09-15 21:30:59 +0800

    侧边栏结束

--- src/components/sandbox/SideMenu.jsx
+++ src/components/sandbox/SideMenu.jsx
@@ -1,40 +1,96 @@
 import {
-    UploadOutlined,
-    UserOutlined,
-    VideoCameraOutlined,
-  } from '@ant-design/icons';
-  import { Layout, Menu } from 'antd';
-  import React, { useState } from 'react';
-  const { Sider } = Layout;
-const SideMenu = () => {
-    const [collapsed, setCollapsed] = useState(false);
-    return ( 
-        <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-          items={[
-            {
-              key: '1',
-              icon: <UserOutlined />,
-              label: 'nav 1',
-            },
-            {
-              key: '2',
-              icon: <VideoCameraOutlined />,
-              label: 'nav 2',
-            },
-            {
-              key: '3',
-              icon: <UploadOutlined />,
-              label: 'nav 3',
-            },
-          ]}
-        />
-      </Sider>
-     );
+  HomeOutlined,
+  SolutionOutlined,
+  UserOutlined,
+  UnlockOutlined,
+  TeamOutlined,
+  KeyOutlined,
+  MailOutlined,
+  FormOutlined,
+  AlignLeftOutlined,
+  RadarChartOutlined,
+  AuditOutlined,
+  HighlightOutlined,
+  OrderedListOutlined,
+  ExclamationCircleOutlined,
+  SnippetsOutlined,
+  CheckCircleOutlined,
+  StopOutlined
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { withRouter } from 'react-router-dom';
+const { Sider } = Layout;
+const iconList = {
+  "/home": <HomeOutlined />,
+  "/user-manage": <SolutionOutlined />,
+  "/user-manage/list": <UserOutlined />,
+  "/right-manage": <UnlockOutlined />,
+  "/right-manage/role/list": <TeamOutlined />,
+  "/right-manage/right/list": <KeyOutlined />,
+  "/news-manage": <MailOutlined />,
+  "/news-manage/add": <FormOutlined />,
+  "/news-manage/draft": <AlignLeftOutlined />,
+  "/news-manage/category": <RadarChartOutlined />,
+  "/audit-manage": <AuditOutlined />,
+  "/audit-manage/audit": <HighlightOutlined />,
+  "/audit-manage/list": <OrderedListOutlined />,
+  "/publish-manage": <SnippetsOutlined />,
+  "/publish-manage/unpublished": <ExclamationCircleOutlined />,
+  "/publish-manage/published": <CheckCircleOutlined />,
+  "/publish-manage/sunset": <StopOutlined />
 }
- 
-export default SideMenu;
+function getItem(label, key, icon, children, type) {
+  let child = [];
+  if (children?.length) {
+    children.forEach(item => {
+      if (item.pagepermisson) {
+        child.push(getItem(item.title, item.key, iconList[item.key]))
+      }
+    })
+  }
+  return {
+    key,
+    icon,
+    children: children?.length ? child : undefined,
+    label,
+    type,
+  };
+}
+const SideMenu = ({history,location}) => {
+  const [items, setitems] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children')
+      .then(res => {
+        let data = res.data;
+        setitems(data.map(item => {
+          if (item.pagepermisson) {
+            return getItem(item.title, item.key, iconList[item.key], item.children)
+          }
+        }))
+      })
+  }, [])
+  return (
+    <Sider>
+      {/* 让侧边栏超出屏幕范围的时候自己滚动，不让整个页面滚动 */}
+      <div style={{ display: 'flex', height: '100%', flexDirection: 'column'}}>
+        <div style={{ flex: '1', overflow: 'auto' }}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            // 储存选中的导航，刷新之后不会消失,
+            // selectedKeys将组件设置为受控组件，以免重新刷新路径跳转回home页
+            selectedKeys={[location.pathname]}
+            // 展开选中的导航，刷新之后不会消失
+            defaultOpenKeys={['/'+location.pathname.split('/')[1]]}
+            items={items}
+            onClick={(item) => history.push(item.key)}
+          />
+        </div>
+      </div>
+    </Sider>
+  );
+}
+
+export default withRouter(SideMenu);
--- src/components/sandbox/TopHeader.jsx
+++ src/components/sandbox/TopHeader.jsx
@@ -2,19 +2,28 @@ import {
     MenuFoldOutlined,
     MenuUnfoldOutlined
   } from '@ant-design/icons';
-  import { Layout, Menu } from 'antd';
+  import { Layout } from 'antd';
   import React, { useState } from 'react';
   const { Header } = Layout;
 const TopHeader = () => {
     const [collapsed, setCollapsed] = useState(false);
+    const changeCollapsed=()=>{
+      setCollapsed(!collapsed)
+    }
+    const back=()=>{
+      localStorage.removeItem('token')
+    }
     return ( 
         <Header
           className="site-layout-background"
           style={{
             padding: '0 15px 0',
+            display:'flex',
+            justifyContent:'space-between'
           }}
         >
-          {collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+          {collapsed ? <MenuUnfoldOutlined onClick={changeCollapsed}/> : <MenuFoldOutlined onClick={changeCollapsed}/>}
+          <div onClick={back}>退出</div>
         </Header>
      );
 }
